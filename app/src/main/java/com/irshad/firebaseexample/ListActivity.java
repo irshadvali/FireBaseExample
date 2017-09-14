e7974f094a0be57b919e1b8119023817aac9f8a0
irshadvali <irshad@xelpmoc.in>
2017-09-14 22:53:05 +0530

    intent

--- app/src/main/java/com/irshad/firebaseexample/ListActivity.java
+++ app/src/main/java/com/irshad/firebaseexample/ListActivity.java
@@ -1,13 +1,24 @@
 package com.irshad.firebaseexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ListActivity extends AppCompatActivity {
-
+ ImageView setting;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+        setting=(ImageView) findViewById(R.id.setting);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
